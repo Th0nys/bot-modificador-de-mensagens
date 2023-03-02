5c09a92f1269355341bded2cbb2320f53c746304
Anthony Matheus Santos Gonçalves <86919567+Th0nys@users.noreply.github.com>
2023-03-02 18:02:51 -0300

    Delete Responses.py

--- Responses.py
+++
@@ -1,8 +0,0 @@
-from telegram.ext import *
-
-
-def sample_responses(update, text):
-    user_message = str(input()).lower()
-
-    if user_message in ("olá"):
-        user_message = update.message.text('hello')

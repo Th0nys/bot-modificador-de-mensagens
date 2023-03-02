1b3a7cc0f8c61fc8e431a82e207f420489481030
Anthony Matheus Santos Gonçalves <86919567+Th0nys@users.noreply.github.com>
2023-03-02 18:04:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,65 @@
-import Constants as keys
-from telegram.ext import *
-import Responses as R
+import requests
+
+bot_token = "6087028385:AAHRBTY2TvIM2yuhZkWOzZvVwwfQ9xwBiC4"
+chat_id = "modificatest.bot"
+msg = "Messege received from chatbot"
 
 
-def start_commands(update, context):
-    update.message.reply_text('')
-    pass
+def send_message(msg):
+   telegram_api_url = f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id=@{chat_id}&text={msg}"
+   tel_resp = requests.get(telegram_api_url)
+   if tel_resp.status_code == 200:
+       print("Mensagem enviada ao grupo..")
+   else:
+       print("Mensagem não pôde ser enviada!!")
+
+var = send_message('outra mensagem 1 2 3 ')
+var
 
 
-def hellp_commands(update, context):
-    update.message.reply_text('')
-    pass
+def message_ind(msg):
+    params = {
+    'offset': 0, 
+    'limit': 10,
+    }
+
+    response = requests.get(f'https://api.telegram.org/bot{bot_token}/getUpdates', params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        for update in data['result']:
+            if 'message' in update and 'from' in update['message'] and update['message']['from']['is_bot']:
+                message_id = update['message']['message_id']
+    
+    return message_id
 
 
-def handle_message(update, context):
-    text = str(update.message.text).lower()
-    response = R.sample_responses(text)
-
-    update.message.reply_text(response)
-
-
-def error(update, context):
-    print(f"Update {update} caused error {context.error}")
-
-
-def main():
-    updater = Updater(keys.API_KEY, use_context=True)
-    updater.start_polling()
+def chat_ind(msg):
+    url = f'https://api.telegram.org/bot{bot_token}/getUpdates'
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        update = response.json()['result'][-1]
+        chat_id = update['message']['chat']['id']
+    
+    return chat_id
+    
 
 
+def edit_message():
+    novo_texto = 'nova mensagem'
+    message_id = message_ind(var)
+    chat_id = chat_ind(var)
+    url = f'https://api.telegram.org/bot{bot_token}/editMessageText'
+    params = {'chat_id': chat_id, 'message_id': message_id, 'text': novo_texto}
+    response = requests.post(url, params=params)
 
+    # Verifique se a solicitação foi bem-sucedida
+    if response.status_code == 200:
+        print('A mensagem foi editada com sucesso!')
+    else:
+        print('Ocorreu um erro ao editar a mensagem.')
 
-
+edit_message()
+print(message_ind(var))
+print(chat_ind(var))
